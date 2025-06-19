7bc33a140e49d97f004fdaf685ff85d56e882e3a
Anna Kurova <annainterior14@gmail.com>
2025-06-19 11:24:42 +0200

    add workspace parameter

--- src/run_script.py
+++ src/run_script.py
@@ -45,7 +45,9 @@ venv_python = os.path.join(os.path.dirname(sys.executable), "python")
 
 REGION_VALUES_1D = [
     [-1, 1], [-0.3, 0.3], [-0.5, 0.5], [0, 1],
-    [-1, 0.5], [-1, 0], [-0.5, 1], [-1, 0.7], [-1, 0.3]
+    [-1, 0.5], [-1, 0], [-0.5, 1], [-1, 0.7], [-1, 0.3],
+    [-0.2, 0.2], [-0.7, 0.7], [-0.8, -0.2], [0.2, 0.8],
+    [0.5, 1], [0.3, 1], [-1, -0.5], [-1, -0.3], [-0.9, 0.9],
 ]
 
 REGION_VALUES_2D = [
@@ -53,21 +55,28 @@ REGION_VALUES_2D = [
     [[-0.5, 0.5], [-0.5, 0.5]],
     [[0.5, 1], [0.5, 1]],
     [[0, 1], [0, 1]],
-    [[-0.3, 0.3], [-0.3, 0.3]]
+    [[-0.3, 0.3], [-0.3, 0.3]],
+    [[-0.5, 1], [-0.5, 1]],
+    [[-1, 0.5], [-0.5, 1]],
+    [[-1, 0], [-1, 0]],
+    [[0.3, 1], [-1, -0.3]],
+    [[-0.8, -0.2], [0.2, 0.8]],
+    [[0.2, 0.8], [-0.8, -0.2]],
+    [[-1, -0.5], [-1, -0.5]],
 ]
 
 # Shared base configs
 BASE_CONFIGS = {
     "poisson_experiment": {
-        # "model": "Poisson_SM",
-        "model": "Poisson_MLE",
-        # "weight_function": "distance",
+        "model": "Poisson_SM",
+        # "model": "Poisson_MLE",
+        "weight_function": "distance",
         "mirror_boundary": False,
         "dist_params": {"n_expected": 100, "scale": 0.5},
         "hidden_dims": [32],
         "patience": 20,
         "learning_rate": 1e-03,
-        "plot_gradients": True,
+        "plot_gradients": False,
         "plot_losses": True,
         "l2_regularization": True,
         "optimizer": "rprop",
@@ -132,16 +141,22 @@ def main():
         ],
         help="Which script to run."
     )
+    parser.add_argument(
+        "--config", type=str, required=True,
+        help="Path to the configuration file."
+    )
+    parser.add_argument("--workspace", type=str, required=True)
     args = parser.parse_args()
 
     config = BASE_CONFIGS[args.script].copy()
+    config["workspace"] = args.workspace
 
     if args.script == "region_optimization":
         config["regions"] = REGION_VALUES_2D
         save_config_and_run(args.script, config, region_key=None)
 
     elif args.script == "poisson_experiment":
-        for i, region in enumerate(REGION_VALUES_2D):
+        for i, region in enumerate(REGION_VALUES_1D):
             save_config_and_run(
                 args.script, config.copy(),
                 region_key="region", region=region, index=i,
